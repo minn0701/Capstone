21ade954aba94178fe814e6573c648724106e90f
love-1006 <castlesum1006@gmail.com>
2025-06-20 03:59:31 +0900

    server2 연동 수정

--- server2/src/main/java/ensm/server2/controller/ForwardController.java
+++ server2/src/main/java/ensm/server2/controller/ForwardController.java
@@ -1,22 +1,14 @@
 package ensm.server2.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ForwardController {
 
-//    @RequestMapping(value = { "/", "/{path:^(?!api$).*$}/**" })
-//    public String forward() {
-//        return "forward:/index.html";
-//    }
-
-    @Controller
-    public class MainController {
-
-        @RequestMapping(value = {"/main", "/main/{path:^(?!static|favicon\\.ico|manifest\\.json).*$}/**"})
-        public String forwardMain() {
-            return "forward:/index.html";
-        }
+    @RequestMapping(value = {"/main", "/main/{path:^(?!static|favicon\\.ico|manifest\\.json).*$}/**"})
+    public String forwardMain() {
+        return "forward:/index.html";
     }
 }
